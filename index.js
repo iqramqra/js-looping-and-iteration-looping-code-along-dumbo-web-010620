f62b3f780ba545fa3be4052414e8c09daf6b189c
Iqra Masroor <masrooriqra@gmail.com>
2020-02-17 20:20:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,3 +5,11 @@ function writeCards( namesArray, event ) {
   }
   return thankYouCards
 }
+
+function countDown( startingNumber ) {
+  while ( startingNumber > 0 ) {
+    console.log( startingNumber );
+    startingNumber -= 1;
+  }
+  console.log( startingNumber );
+}
