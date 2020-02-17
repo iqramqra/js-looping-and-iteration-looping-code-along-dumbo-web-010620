3107864288db93d4decf7f3e3b8606c05fe02908
Iqra Masroor <masrooriqra@gmail.com>
2020-02-17 20:21:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-function writeCards( names, event ) {
+function writeCards(names, event ) {
   let thankYouCards = []
   for ( let i = 0; i < names.length; i++ ) {
     thankYouCards.push( `Thank you, ${names[i]}, for the wonderful ${event} gift!` )
